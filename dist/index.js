d6e623a686e6d9fd2811d0e7130601a732a9a2b3
Domenic Gerhold <gerhold@helllicht.com>
2021-01-13 14:07:46 +0100

    Fix match bug;

--- dist/index.js
+++ dist/index.js
@@ -26957,12 +26957,6 @@ let prefixRepair = function (path) {
             return resolve(path);
         }
 
-        const regexOnlyLetter = /^[A-Za-z]+$/;
-        if (path.charAt(0).value.match(regexOnlyLetter)) {
-            // 'example'
-            return resolve('./' + path);
-        }
-
         if (path.startsWith('/')) {
             // '/example'
             core.warning('prefixRepair.js - it is not allowed to start path with "/" - script prefixed it with "."')
@@ -26976,8 +26970,7 @@ let prefixRepair = function (path) {
             return resolve('./' + path.slice(1, 99));
         }
 
-        core.setFailed('Error from prefixRepair.js - no exit condition found!');
-        return reject();
+        return resolve('./' + path);
     });
 };
 
--- prefixRepair.js
+++ prefixRepair.js
@@ -26,12 +26,6 @@ let prefixRepair = function (path) {
             return resolve(path);
         }
 
-        const regexOnlyLetter = /^[A-Za-z]+$/;
-        if (path.charAt(0).value.match(regexOnlyLetter)) {
-            // 'example'
-            return resolve('./' + path);
-        }
-
         if (path.startsWith('/')) {
             // '/example'
             core.warning('prefixRepair.js - it is not allowed to start path with "/" - script prefixed it with "."')
@@ -45,8 +39,7 @@ let prefixRepair = function (path) {
             return resolve('./' + path.slice(1, 99));
         }
 
-        core.setFailed('Error from prefixRepair.js - no exit condition found!');
-        return reject();
+        return resolve('./' + path);
     });
 };
 
